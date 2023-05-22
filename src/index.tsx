103b9e216a218de85eb9164ef8a49dba15c7a8d0
DaYuan Lin <dayuanlin@oursong.com>
2023-05-22 10:53:16 +0800

    chore: fix

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,14 @@ const MyComponent = () => {
   return (
     <View>
       <Svg height="100" width="100">
-        <Circle cx="50" cy="50" r="40" stroke="black" strokeWidth="2" fill="red" />
+        <Circle
+          cx="50"
+          cy="50"
+          r="40"
+          stroke="black"
+          strokeWidth="2"
+          fill="red"
+        />
       </Svg>
     </View>
   );
